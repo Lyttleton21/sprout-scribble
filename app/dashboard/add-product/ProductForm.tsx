3108406b6cca8895d8dd218864386d9e55f8c177
Damilare <bolarinwaoluwadamilare91@gmail.com>
2024-09-16 15:56:46 +0100

    Create Product Form

---
+++ app/dashboard/add-product/ProductForm.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { ProductSchema } from "@/types/product-schema";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { DollarSign } from "lucide-react";
+
+export default function ProductForm() {
+  const form = useForm<z.infer<typeof ProductSchema>>({
+    defaultValues: {
+      title: "",
+      description: "",
+      price: 0,
+    },
+  });
+
+  const onSubmit = (value: z.infer<typeof ProductSchema>) => {
+    console.log(value);
+    // execute(value);
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Saekdong Stripe" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    {/* <Tiptap val={field.value} /> */}
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Price</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-1">
+                      <DollarSign
+                        size={36}
+                        className="p-2 bg-muted rounded-md"
+                      />
+                      <Input
+                        {...field}
+                        type="number"
+                        placeholder="Your price in USD"
+                        step="0.1"
+                        min={0}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              className="w-full"
+              disabled={
+                status === "executing" ||
+                !form.formState.isValid ||
+                !form.formState.isDirty
+              }
+              type="submit"
+            >
+              {/* {editMode ? "Save Changes" : "Create Product"} */}
+              Create Product
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,5 +1,11 @@
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
 import React from "react";
+import ProductForm from "../add-product/ProductForm";
 
-export default function page() {
-  return <div>page</div>;
+export default async function page() {
+  const session = await auth();
+  if (session?.user.role !== "admin") redirect("/dashboard/settings");
+
+  return <div>Products Page</div>;
 }
